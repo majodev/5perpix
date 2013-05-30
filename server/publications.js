e500ec18bb39e5e70dfa08acf602b20d256702bc
majodev <contact@majodev.com>
2013-05-30 04:25:12 +0200

    more structure, comment cleanup

--- server/publications.js
+++ server/publications.js
@@ -2,10 +2,6 @@
 	* Publish behaviour and CRUD on server
 	* package autopublish was removed from meteor: we need to define our own publish and subscribe behaviour!
 	* package insecure was removed from meteor: we need to define the allowed actions on the data (pictures and pixels)
-	*
-	* sum-up:
-	* @pictures: always publish all pictures to all users. 
-	* @pixels: always publish all pixels of all pictures! (client will only subscribe to the pixels it currently needs!).
 	*/
 
 	Meteor.publish("mrtpicturecollection", function () {
